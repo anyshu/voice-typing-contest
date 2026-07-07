f5c3a8c57a5dd8749ddc9086c2576d6b316d6971
anyshu <anyshu@163.com>
2026-07-07 18:51:08 +0800

    fix: 修复 electron-updater 的 CommonJS 导入问题
    使用默认导入而不是命名导入来兼容 ESM 项目

--- src/main/auto-updater.ts
+++ src/main/auto-updater.ts
@@ -1,4 +1,5 @@
-import { autoUpdater } from "electron-updater";
+import pkg from "electron-updater";
+const { autoUpdater } = pkg;
 import { BrowserWindow, dialog } from "electron";
 import log from "electron-log";
 
